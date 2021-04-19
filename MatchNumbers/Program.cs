60361cab6e1f4e56dfb1e223f80723d13ae48f80
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-04-19 04:48:57 +0300

    Add project files.

---
+++ MatchNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( match("1221441221") );
+        }
+
+        public static bool match(string source)
+        {
+            int sum = 0;
+            for (int i = 0; i < source.Length; i++)
+            {
+                int counter = 0;
+                for (int j = i; j < source.Length; j++)
+                {
+                    if (source[i] != source[j]) counter++;
+
+
+                    else if(source[i] == source[j] && counter % 2 != 0)
+                    {
+                        return false;
+                    }
+
+                }
+
+                sum++;
+            }
+            if (sum % 2 != 0) return false;
+            return true;
+        }
+    }
+}
